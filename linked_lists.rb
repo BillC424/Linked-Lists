be209e7f70bd786cdc772e5eb1856c75613ad74f
BillC424 <wcurran44@gmail.com>
2022-10-15 23:43:39 -0400

    Updated append method to iterate through every node next_node instance variable until the last node is reached. After the last node is reached, the new node is added

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,70 +1,92 @@
 class LinkedList
 
-  attr_accessor :head, :tail
-
-  def initialize
-    @head = nil
-    @tail = nil
-  end 
-
-  def append(value)
-    new_node = Node.new(value)
-    return @head = new_node if @head = nil 
-    current_node = @head
-    while current_node != nil
-      current_node = curren_node.next  
+    def initialize
+      @head = nil
+      @tail = nil
+    end 
+  
+    def append(value)
+      #creates node
+      new_node = Node.new(value)
+      #adds node to head variable if empty
+      @head = new_node if @head == nil 
+      #assigns @head value to current node local variable
+      current_node = @head
+      #exit method if there's only one node 
+      return if current_node.next_node == nil
+      #loop to iterate through linked list 
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        if current_node.next_node == nil
+          current_node.next_node = new_node
+        end
+      end
     end
-  end
-
-  def prepend(value)
-    new_node = Node.new(value)
-    new_node.next_node = @head.value
-    @head = new_node
-  end
-
-  def size
-
-  end 
-
-  def head
-
-  end 
-
-  def tail 
-
-  end 
-
-  def at(index)
-
-  end 
-
-  def pop
+  
+    def prepend(value)
+      new_node = Node.new(value)
+      unless @head == nil then new_node.next_node = @head
+      end
+      @head = new_node
+    end
+  
+    def size
     
-  end 
-
-  def contains?(index)
-
-  end 
-
-  def find(value)
-
-  end 
-
-  def to_s
-
-  end 
-
-
-end
-
-class Node
-
-  attr_accessor :value, :next_node  
-
-  def initialize(value = nil, next_node = nil)  
-    @value = value
-    @next_node = next_node
+    end 
+  
+    def head
+      @head
+    end 
+  
+    def tail 
+      @tail
+    end 
+  
+    def at(index)
+  
+    end 
+  
+    def pop
+      
+    end 
+  
+    def contains?(index)
+  
+    end 
+  
+    def find(value)
+  
+    end 
+  
+    def to_s
+  
+    end 
+  
+  
   end
-
-end
-
+  
+  class Node
+  
+    attr_accessor :value, :next_node  
+  
+    def initialize(value = nil, next_node = nil)  
+      @value = value
+      @next_node = next_node
+    end
+  
+  end
+  
+  list = LinkedList.new
+  
+  list.append(44)
+   list
+  list.prepend(55)
+   list
+  list.prepend(66)
+   list
+  list.append(77)
+   p list
+  
+  
+  
+  
